7217f1caf240817e2dfd7c15de92b249c20e3126
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-19 22:57:12 +0700

    add e2e config and test for favoriting restaurant

---
+++ e2e/Liking_Restaurants.spec.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+
+/* eslint-disable no-undef */
+Feature('Favoriting Restaurants');
+
+Before(({ I }) => {
+  I.amOnPage('/#/favorite');
+});
+
+Scenario('favoriting one restaurant', async ({ I }) => {
+  I.see("You don't have any favorite restaurant!", 'error-message');
+
+  I.amOnPage('/');
+
+  I.seeElement('.restaurant-item .card-title > a.card-link');
+
+  const firstRestaurant = locate('.restaurant-item .card-title > a.card-link').first();
+  const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
+
+  I.seeElement('#favoriteButton');
+  I.click('#favoriteButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.restaurant-item');
+  const favoritedRestaurantTitle = await I.grabTextFrom('.card-title');
+
+  assert.strictEqual(firstRestaurantTitle, favoritedRestaurantTitle);
+});
